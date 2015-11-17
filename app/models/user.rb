e6ec9e94977fdea8128c6740af149538d2fd6ac5
DewaldoDev <dewaldodev@gmail.com>
2015-11-17 16:31:49 -0500

    2015.11.17 Finished Sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+	has_secure_password
+
+	validates :first_name, :last_name, :email, presence: true
+
 	has_many :reservations
 	has_many :restaurants, through: :reservations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'restaurants#index'
 
   resources :restaurants, only: [:index, :show]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
