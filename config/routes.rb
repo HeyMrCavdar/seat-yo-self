5372a8b3cf21c76caee50854385d2b29b94f7144
DewaldoDev <dewaldodev@gmail.com>
2015-11-18 15:19:11 -0500

    Finalized user and session controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'restaurants#index'
 
   resources :restaurants, only: [:index, :show]
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
 
 end
