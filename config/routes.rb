59b58dd8cc4cef087ddb59e54f493e842d8e702f
Matt <matt.cavdar@gmail.com>
2015-11-18 16:27:31 -0500

    Add first and last name to top of show user page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'restaurants#index'
 
   resources :restaurants, only: [:index, :show]
-  resources :users, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :destroy, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
 end
